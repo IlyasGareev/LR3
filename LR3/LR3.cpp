97c9bd6f37284972e64b6033a91df7e5108944f3
IlyasGareev <gareev_ilyas@list.ru>
2022-01-22 17:24:53 +0500

    Добавил хранилище

--- LR3/LR3.cpp
+++ LR3/LR3.cpp
@@ -76,7 +76,82 @@ public:
     }
 };
 
+class MyStorage
+{
+private:
+    Things** object;                    //массив указателей на объекты типа Things
+    int size;
+public:
+    MyStorage(int i)
+    {
+        size = i;
+        object = new Things * [size];
+    }
+    void SetObject(int i, Things* obj)     //установка 
+    {
+        object[i] = obj;
+    }
+    Things& GetObject(int i)
+    {
+        return *object[i];
+    }
+    int getCount()                        //возвращение размера
+    {
+        return size;
+    }
+    void AddObject(Things* obj)
+    {
+        Things** NewObj = new Things * [size + 1];
+        if (size != 0) {
+            for (int i = 0; i < size; i++)
+            {
+                NewObj[i] = object[i];
+            }
+            delete[] object;
+        }
+        object = NewObj;
+        object[size] = obj;
+        size = size + 1;
+    }
+    void delObject(int i)
+    {
+        object[i] = NULL;
+    }
+    void DeleteObject(int i)
+    {
+        if (size != 0 && i < size && size != 1) {
+            Things** NewObj = new Things * [size - 1];
+            for (int j = 0; j < i; j++)
+            {
+                NewObj[j] = object[j];
+            }
+            for (int j = i; j < size - 1; j++)
+            {
+                NewObj[j] = object[j + 1];
+            }
+            delete[] object;
+            size = size - 1;
+            object = NewObj;
+        }
+        else if (size == 1) {
+            object[0] = nullptr;
+            size = 0;
+        }
+    }
+    void PrintNameObject(int i) {
+        if (size != 0 && size > i) {
 
+            object[i]->PrintName();
+        }
+    }
+    ~MyStorage()
+    {
+        for (int i = 0; i < size; i++) {
+            delete object[i];
+        }
+        delete[] object;
+    }
+};
 
 
 int main()
